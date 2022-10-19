7177e685badd47c082fe148080c9678d2f2353d7
Danny Willems <be.danny.willems@gmail.com>
2022-10-19 21:26:49 +0200

    Fix lint

--- example/example_client.py
+++ example/example_client.py
@@ -11,7 +11,7 @@ CLIENT = Client(api_key=API_KEY)
 
 def example_get_host_filter_plugin():
     response = CLIENT.get_host(ipv4="33.33.33.33")
-    assert(response.status_code() == 200)
+    assert response.status_code() == 200
 
 
 def example_get_service_filter_plugin():
@@ -21,7 +21,7 @@ def example_get_service_filter_plugin():
     """
     query_http_ntlm = MustQuery(field=PluginField(Plugin.HttpNTLM))
     response = CLIENT.get_service(queries=[query_http_ntlm])
-    assert(response.status_code() == 200)
+    assert response.status_code() == 200
     # check we only get NTML related services
     assert all((i.tags == ["ntlm"] for i in response.json()))
 
@@ -30,7 +30,7 @@ def example_get_leaks_filter_multiple_plugins():
     query_http_ntlm = MustQuery(field=PluginField(Plugin.HttpNTLM))
     query_country = MustQuery(field=CountryField("France"))
     response = CLIENT.get_service(queries=[query_http_ntlm, query_country])
-    assert(response.status_code() == 200)
+    assert response.status_code() == 200
     assert all(
         (
             i.geoip.country_name == "France" and i.tags == ["ntlm"]
@@ -43,7 +43,7 @@ def example_get_leaks_multiple_filter_plugins_must_not():
     query_http_ntlm = MustQuery(field=PluginField(Plugin.HttpNTLM))
     query_country = MustNotQuery(field=CountryField("France"))
     response = CLIENT.get_service(queries=[query_http_ntlm, query_country])
-    assert(response.status_code() == 200)
+    assert response.status_code() == 200
     assert all(
         (
             i.geoip.country_name != "France" and i.tags == ["ntlm"]
